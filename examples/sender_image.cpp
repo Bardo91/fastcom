4b65b7f20f18dadb930748d18c7ca039865eeecf
Bardo91 <19Bardo91@gmail.com>
2020-05-01 13:03:34 +0200

    added image example but it is not working

---
+++ examples/sender_image.cpp
@@ -0,0 +1,55 @@
+//---------------------------------------------------------------------------------------------------------------------
+//  FASTCOM
+//---------------------------------------------------------------------------------------------------------------------
+//  Copyright 2020 -    Manuel Perez Jimenez (a.k.a. manuoso)
+//                      Marco A. Montes Grova (a.k.a. mgrova) 
+//                      Pablo Ramon Soria (a.k.a. Bardo91)
+//                      Ricardo Lopez Lopez (a.k.a. ric92)
+//---------------------------------------------------------------------------------------------------------------------
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+//  and associated documentation files (the "Software"), to deal in the Software without restriction, 
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial 
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------
+
+
+#include <fastcom/Publisher.h>
+#include <fastcom/Subscriber.h>
+
+#include <fastcom/ConnectionManager.h>
+
+#include <string>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+#include "SerializableMat.h"
+#include <opencv2/opencv.hpp>
+
+
+int main(){
+
+    fastcom::Publisher<SerializableMat> p1("/beauty_face");
+    
+    cv::VideoCapture camera(0);
+
+    cv::Mat oriImg;
+    while (true) {
+        SerializableMat image(oriImg, 50);
+        camera >> image;
+        p1.publish(image);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    
+
+}
